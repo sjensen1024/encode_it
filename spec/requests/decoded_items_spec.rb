b29efe8f3c53c5001dd3c0f46a849f43bf3e72a4
sjensen1024 <stevenmjensen1024@gmail.com>
2025-02-12 20:40:09 -0600

    Fixed the expectations in the request specs

--- spec/requests/decoded_items_spec.rb
+++ spec/requests/decoded_items_spec.rb
@@ -1,26 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe "/decoded_items", type: :request do
-    let(:valid_attributes) do
-        {
-            descriptor: 'Some descriptor',
-            value: 'Hello world!'
-        }
+  let(:valid_attributes) do
+    {
+      descriptor: 'Some descriptor',
+      value: 'Hello world!'
+    }
+  end
+  let(:encoded_item) { FactoryBot.create(:encoded_item, valid_attributes) }
+
+  before { allow(EncodedItem).to receive(:does_item_with_main_descriptor_exist?).and_return(true) }
+
+  describe "GET /index" do
+    it_behaves_like "it redirects if an encoded item with the main descriptor doesn't exist yet" do
+      let(:call_that_should_redirect) { get decoded_items_url }
     end
 
-    describe "GET /index" do
-        it "renders a successful response" do
-          EncodedItem.create! valid_attributes
-          get decoded_items_url
-          expect(response).to be_successful
-        end
-      end
+    it "renders a successful response under normal circumstances" do
+      encoded_item
+      get decoded_items_url
+      expect(response).to be_successful
+    end
+  end
 
-      describe "GET /show" do
-        it "renders a successful response" do
-          encoded_item = EncodedItem.create! valid_attributes
-          get decoded_item_url(encoded_item)
-          expect(response).to be_successful
-        end
-      end
+  describe "GET /show" do
+    it_behaves_like "it redirects if an encoded item with the main descriptor doesn't exist yet" do
+      let(:call_that_should_redirect) { get decoded_item_url(encoded_item) }
+    end
+
+    it "renders a successful response under normal circumstances" do
+      get decoded_item_url(encoded_item)
+      expect(response).to be_successful
+    end
+  end
 end
