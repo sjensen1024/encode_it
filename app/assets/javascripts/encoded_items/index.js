bccd36f16ed82b971a105df2cc6d7ffac9efe0e7
sjensen1024 <stevenmjensen1024@gmail.com>
2025-02-23 08:50:47 -0600

    made alerts into modal dialogs; loading mask without any special css or animation yet

--- app/assets/javascripts/encoded_items/index.js
+++ app/assets/javascripts/encoded_items/index.js
@@ -1,23 +1,55 @@
 function showMainSecretEntryDialog() {
-    document.getElementById('mainSecretEntryDialog').showModal();
+    document.getElementById("mainSecretEntryDialog").showModal();
 };
 
 function hideMainSecretEntryDialog() {
-    document.getElementById('mainSecretEntryDialog').close();
+    document.getElementById("mainSecretEntryDialog").close();
+}
+
+function showAccessDeniedDialog() {
+    document.getElementById("accessDeniedDialog").showModal();
+}
+
+function hideAccessDeniedDialog() {
+    document.getElementById("accessDeniedDialog").close();
+}
+
+function showLoadingMaskDialog(){
+    document.getElementById("loadingMaskDialog").showModal();
+}
+
+function hideLoadingMaskDialog(){
+    document.getElementById("loadingMaskDialog").close();
+}
+
+function reopenSecretEntryFromAccessDenied() {
+    hideAccessDeniedDialog();
+    showMainSecretEntryDialog();
+}
+
+function getMainSecretEntry(){
+    return document.getElementById("mainSecretEntryText").value;
+}
+
+function clearMainSecretEntry(){
+    document.getElementById("mainSecretEntryText").value = "";
 }
 
 function processMainSecretEntrySubmission() {
-    entryBoxValue = document.getElementById('mainSecretEntryText').value;
+    showLoadingMaskDialog();
 
     let requestObject = new XMLHttpRequest();
-    let url = '/decoded_items?main_secret_entry=' + entryBoxValue;
+    let url = '/decoded_items?main_secret_entry=' + getMainSecretEntry();
     requestObject.open("GET", url, true);
+
     requestObject.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
+            clearMainSecretEntry();
             parsedResponse = JSON.parse(this.responseText);
 
             if (!parsedResponse.allowed) {
-                alert('You did not enter the correct value for the main secret. Decoding failed.');
+                hideLoadingMaskDialog();
+                showAccessDeniedDialog();
                 return;
             }
 
@@ -25,6 +57,9 @@ function processMainSecretEntrySubmission() {
                 decodedItem = parsedResponse.decoded_items[i];
                 element = document.getElementById('decoded_item_' + decodedItem.id);
                 element.innerHTML = decodedItem.value;
+                if (i == parsedResponse.decoded_items.length - 1){
+                    hideLoadingMaskDialog();
+                }
             }
         }
     }
